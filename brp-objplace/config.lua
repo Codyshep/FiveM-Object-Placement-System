a8866cf450319c878c35679640edaf60c023a6d8
EagleDevs <58715617+Codyshep@users.noreply.github.com>
2024-05-10 18:12:53 -0500

    Update config.lua

--- brp-objplace/config.lua
+++ brp-objplace/config.lua
@@ -6,7 +6,8 @@ config = {
         {name = 'walllight2', prop = 'apa_mp_stilts_a_stairs_light003'},
         {name = 'cabinet', prop = 'v_corp_lowcabdark01'},
         {name = 'floorlight', prop = 'ba_work_light_clutter001'},
-        {name = 'storage', prop = 'v_ret_ml_liqshelfd', pheight = 1} 
+        {name = 'storage', prop = 'v_ret_ml_liqshelfd', pheight = 1},
+        {name = 'table', prop = 'prop_picnictable_02'}
     }
 }
 
@@ -16,11 +17,12 @@ name = spawn code for the command /p, for example "/p [PlayerID] [spawn code]"
 
 prop = the prop that will be getting spawned, you can find prop spawn names here https://forge.plebmasters.de/objects
 
-pheight = sometimes the objects holograms before the placements will be shown in the ground even though they arent, 
+pheight = sometimes the objects holograms before the placements will be shown in the ground even though they aren't, 
 adjusting the height will make the object hologram appear higher ultimately removing the phasing through the ground look.
 
 NOTE: keep in mind you only need [name] & [prop] the other variables don't need to be added unless you want to adjust values
 or enable a feature.
+
 ]]--
 
 
